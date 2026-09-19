ad9f7f278b4ba8302fd8b12e836c77e5b945b46f
basaihinn-prog <basaihinn@gmail.com>
2026-09-19 12:04:48 +0630

    Fail cleanly when provider session bootstrap fails

--- .wainwright/casino-dog/src/Controllers/Game/Netent/NetentMain.php
+++ .wainwright/casino-dog/src/Controllers/Game/Netent/NetentMain.php
@@ -20,6 +20,11 @@ class NetentMain extends GameKernel
         $token = $data['token_internal'];
         $session = new NetentSessions();
         $game_content = $session->create_session($token);
+
+        if ($game_content === false || $game_content === null || $game_content === '') {
+            return false;
+        }
+
         return $this->game_launch($game_content);
     }
 
